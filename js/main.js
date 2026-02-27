c970f769d34d80a29f22dd94f52610617a904f47
nanax1107 <nanaito1170@gmail.com>
2026-02-27 14:17:58 +0900

    修正３

--- js/main.js
+++ js/main.js
@@ -34,6 +34,12 @@ $(function($){
             {
                 breakpoint: 768,  
                 settings: {
+                    slidesToShow: 2, 
+                }
+            },
+            {
+                breakpoint: 480,  
+                settings: {
                     slidesToShow: 1, 
                 }
             }
